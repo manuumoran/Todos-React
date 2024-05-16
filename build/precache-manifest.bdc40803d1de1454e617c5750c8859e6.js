24924f0347cbfd918d4809774f600952508b06bf
manuumoran <manu.moran0710@gmail.com>
2024-05-16 16:31:58 -0300

    casual commit

---
+++ build/precache-manifest.bdc40803d1de1454e617c5750c8859e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1da270cb49ecc6d5e663ea825ad6b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab72e694a3d6a0aa01a4",
+    "url": "/static/css/main.95ec0917.chunk.css"
+  },
+  {
+    "revision": "dd6126a7663d2708481b",
+    "url": "/static/js/2.6ca80ecc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.6ca80ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab72e694a3d6a0aa01a4",
+    "url": "/static/js/main.e5da4cf1.chunk.js"
+  },
+  {
+    "revision": "c69c5a55a64c3288d476",
+    "url": "/static/js/runtime-main.ce5510cc.js"
+  },
+  {
+    "revision": "cf1510bcfa5c0ca23868af85b291edee",
+    "url": "/static/media/check.cf1510bc.svg"
+  },
+  {
+    "revision": "0fc1e19b5ef0a87cae5d46ef529a743c",
+    "url": "/static/media/delete.0fc1e19b.svg"
+  }
+]);
