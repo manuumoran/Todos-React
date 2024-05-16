0b6e8cb0483ff4acb8b05098796c5879b74cbe22
manuumoran <manu.moran0710@gmail.com>
2024-05-16 14:51:34 -0300

    Cambio de props a useContext

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,7 +1,14 @@
+import { TodoContext } from '../TodoContext';
 import './TodoSearch.css';
 import React from 'react';
 
-function TodoSearch({ searchValue, setSearchValue, loading }) {
+function TodoSearch() {
+    const {
+        searchValue,
+        setSearchValue,
+        loading
+    } = React.useContext(TodoContext)
+
     return (
         <input 
             placeholder={!loading && 'Search for a TODO'}
--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import { LuLoader } from "react-icons/lu";
 import './TodosLoading.css'
+import React from 'react';
 
 function TodosLoading() {
     return (
